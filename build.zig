35318a43397dfdc47edee24d3fe88e76e20f2695
chaosratel <138471433+kiffpuppygames@users.noreply.github.com>
2024-11-10 13:41:42 +0000

    Add static library definition for kiff-common and update package name

--- build.zig
+++ build.zig
@@ -5,6 +5,17 @@ pub fn build(b: *std.Build) void
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    // Define the library
+    const lib = b.addStaticLibrary(.{
+        .name = "kiff-common",
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    
+    lib.root_module.addImport("kiff-common", b.addModule("kiff-common", .{ .root_source_file = b.path("src/root.zig") } ));
+    b.installArtifact(lib);
+
     // Define the test step
     const debug_unit_tests = b.addTest(.{
         .name = "common-tests",
