d7f403c9ba98f07641631e337383c1afbd20b480
chaosratel <138471433+kiffpuppygames@users.noreply.github.com>
2024-11-10 13:32:31 +0000

    Update launch configuration and task settings; refactor build steps for unit tests

--- build.zig
+++ build.zig
@@ -5,25 +5,16 @@ pub fn build(b: *std.Build) void
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "common",
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    b.installArtifact(lib);
-
-    const lib_unit_tests = b.addTest(.{
+    // Define the test step
+    const debug_unit_tests = b.addTest(.{
         .name = "common-tests",
         .root_source_file = b.path("src/tests.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+    b.installArtifact(debug_unit_tests);
 
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
-
+    const build_tests_step = b.step("test", "Build unit tests");
+    build_tests_step.dependOn(&debug_unit_tests.step);
 }
